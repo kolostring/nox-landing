d6fb95c9fe57cc40c497b5299212b6f65199f61f
kolostring <yoerodriguezjunior@gmail.com>
2024-07-20 02:57:58 -0400

    feat: add skeletons to Post Preview Component

--- src/components/PostPreview.tsx
+++ src/components/PostPreview.tsx
@@ -5,6 +5,7 @@ import {
   GridItem,
   Heading,
   Image,
+  Skeleton,
   Stack,
   Tag,
   Text,
@@ -16,9 +17,51 @@ import DiagonalArrow from "@/assets/diagonal_arrow.svg";
 export type VerticalPostProps = {
   post: PostInfo | undefined;
   isHorizontal?: boolean | undefined;
+  isLoading: boolean;
 };
 
-export default function PostPreview({ post, isHorizontal }: VerticalPostProps) {
+export default function PostPreview({
+  post,
+  isHorizontal,
+  isLoading,
+}: VerticalPostProps) {
+  if (isLoading) {
+    return (
+      <Grid
+        minH={isHorizontal ? "180px" : "434px"}
+        templateColumns={
+          isHorizontal ? { base: "1fr 2fr", lg: "19fr 17fr" } : undefined
+        }
+        templateRows={isHorizontal ? undefined : "19fr 17fr"}
+      >
+        <GridItem as={Skeleton} isLoaded={!isLoading} />
+        <GridItem
+          pt={isHorizontal ? undefined : "32px"}
+          pl={isHorizontal ? "24px" : undefined}
+        >
+          <Skeleton fontSize="14px" h="1.2em" w="10ch" isLoaded={!isLoading} />
+          <Skeleton
+            mt="12px"
+            fontSize={isHorizontal ? "18px" : "24px"}
+            h="1.1em"
+            w="10ch"
+            isLoaded={!isLoading}
+          />
+          <Skeleton
+            mt={isHorizontal ? "8px" : "12px"}
+            h="60px"
+            isLoaded={!isLoading}
+          />
+          <Flex mt="24px" gap="8px">
+            <Skeleton h="24px" w="6ch" rounded="full" isLoaded={!isLoading} />
+            <Skeleton h="24px" w="10ch"rounded="full"  isLoaded={!isLoading} />
+            <Skeleton h="24px" w="8ch" rounded="full" isLoaded={!isLoading} />
+          </Flex>
+        </GridItem>
+      </Grid>
+    );
+  }
+
   if (post === undefined) {
     return <>Unknown Post</>;
   }
